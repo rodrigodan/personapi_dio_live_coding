d64750b45fad314a6b45597f10803510237d9913
rgdaniel <rgdaniel@indracompany.com>
2021-11-03 00:35:48 -0300

    developed get person by id

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -42,8 +43,12 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = verifyIfExists(id);
-
+        //Person person = verifyIfExists(id);
+        Person person = personRepository.findById(id).
+                orElseThrow(()->new PersonNotFoundException(id));
+        /*if(optionalPerson.isEmpty()){
+            throw new PersonNotFoundException(id);
+        }*/
         return personMapper.toDTO(person);
     }
 
